18da695cf77a7950494ca34a50148a49ca555f61
shubham gupta <shubham.gupta2k11@gmail.com>
2022-07-14 22:25:47 +0530

    spring autherization server

---
+++ src/main/java/com/bharath/springcloud/security/AuthserverjwtApplication.java
@@ -0,0 +1,13 @@
+package com.bharath.springcloud.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserverjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserverjwtApplication.class, args);
+	}
+
+}
